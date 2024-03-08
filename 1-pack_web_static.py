93a071b4788f12d14ad2d108a5beab17dfe35577
AbdelhayZaadaddi <132441947+AbdelhayZaadaddi@users.noreply.github.com>
2024-03-08 17:31:19 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,24 @@
-#!/usr/bin/python3
-from fabric.api import local
-from time import strftime
-from datetime import date
+from fabric import task
+from datetime import datetime
+import os
 
-
-def do_pack():
-    """ A script that generates archive the contents of web_static folder"""
-
-    filename = strftime("%Y%m%d%H%M%S")
+@task
+def do_pack(c):
+    """Generates a .tgz archive from the contents of the web_static folder."""
     try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
+        # Create the versions folder if it doesn't exist
+        if not os.path.exists("versions"):
+            c.local("mkdir versions")
 
-        return "versions/web_static_{}.tgz".format(filename)
+        # Generate the archive name using the current timestamp
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_name = f"web_static_{timestamp}.tgz"
 
+        # Pack the contents of web_static into the archive
+        c.local(f"tar -cvzf versions/{archive_name} web_static")
+
+        # Return the path of the generated archive
+        return f"versions/{archive_name}"
     except Exception as e:
+        print(f"Error: {e}")
         return None
