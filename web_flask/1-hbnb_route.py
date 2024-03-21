92caaee6dd7285a49ceae155724a2be2261832eb
Abdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-03-21 06:16:25 +0000

    koko

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starts a simple Flask application """
+""" Starts a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def hello_hbnb():
     """ Displays Hello HBNB! """
     return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """ Displays HBNB """
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
