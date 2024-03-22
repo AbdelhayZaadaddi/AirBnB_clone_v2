4ab07995c0d1ef1facb1e72f8d3cfe566df709d0
Abdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-03-22 15:27:54 +0000

    task 3?

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,35 +1,34 @@
 #!/usr/bin/python3
-""" Starts a Flask web application """
+""" Flask application """
+
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Displays Hello HBNB! """
+def hello():
+    """ say hello """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """ Displays HBNB """
+def hbnb():
+    """ show HBNB """
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
-    """ Displays C followed by the value of the text variable """
-    return 'C {}'.format(text.replace('_', ' '))
-
-
-@app.route('/python/', default={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def display_python(text):
-    """ Displays Python followed by the value of the text variable """
-    return 'Python {}'.format(text.replace('_', ' '))
+def python(text='is_cool'):
+    """ show variable text """
+    return f'Python {text.replace("_", " ")}'
+
+
+@app.route('/number/<int:n>')
+def check_integer(n):
+    return (f"{n} is an integer")
 
 
 if __name__ == '__main__':
+    app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
