a89288d7d047875764e82ad40369f6679151ccb8
Abdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-03-23 13:23:44 +0000

    lppl

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ new class for sqlAlchemy """
+from os import getenv
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base
 from models.state import State
 from models.city import City
 from models.user import User
 from models.place import Place
 from models.review import Review
 from models.amenity import Amenity
-from os import getenv
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import Base
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import (create_engine)
 
 
 class DBStorage:
@@ -19,15 +19,17 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        self.__engine = create_engine(
-            'mysql+mysqldb://{}:{}@{}/{}' .format(
-                getenv("HBNB_MYSQL_USER"),
-                getenv("HBNB_MYSQL_PWD"),
-                getenv("HBNB_MYSQL_HOST"),
-                getenv("HBNB_MYSQL_DB")),
-            pool_pre_ping=True)
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
 
-        if getenv("HBNB_ENV") == "test":
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+
+        if env == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
@@ -37,7 +39,7 @@ class DBStorage:
         """
         dic = {}
         if cls:
-            if isinstance(cls, str):
+            if type(cls) is str:
                 cls = eval(cls)
             query = self.__session.query(cls)
             for elem in query:
@@ -57,16 +59,16 @@ class DBStorage:
         """
         self.__session.add(obj)
 
+    def save(self):
+        """save changes
+        """
+        self.__session.commit()
+
     def delete(self, obj=None):
         """delete an element in the table
         """
         if obj:
             self.session.delete(obj)
-
-    def save(self):
-        """save changes
-        """
-        self.__session.commit()
 
     def reload(self):
         """configuration
@@ -77,5 +79,6 @@ class DBStorage:
         self.__session = Session()
 
     def close(self):
-        """ close session """
+        """ calls remove()
+        """
         self.__session.close()
