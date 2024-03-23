13d6e38b52de4cee05c463ab75fb8f5bc13ef999
Abdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-03-23 13:11:49 +0000

    task 07

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""Flask web application"""
-
-
+""" Script that runs an app with Flask framework """
 from flask import Flask, render_template
 from models import storage
-from models import *
 from models.state import State
+
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ show states """
-    States = storage.all(State)
-    return render_template('7-states_list.html', states=States)
-
-
 @app.teardown_appcontext
-def tear_db(exception):
-    """close storage"""
-    return storage.close()
+def teardown_session(exception):
+    """ Teardown """
+    storage.close()
 
 
-if __name__ == '__main__':
+@app.route('/states_list', strict_slashes=False)
+def display_html():
+    """ Function called with /states_list route """
+    states = storage.all(State)
+    dict_to_html = {value.id: value.name for value in states.values()}
+    return render_template('7-states_list.html',
+                           Table="States",
+                           items=dict_to_html)
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
