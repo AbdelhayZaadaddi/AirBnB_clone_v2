d1a4269076fd318edfbec3e3a96440af632a790b
Abdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-03-24 07:10:53 +0000

    lll

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-""" module numbr roote """
+""" module numbr roote"""
 from flask import Flask
 from flask import render_template
 from models import storage
 from models.state import State
 from models import *
+from models.city import City
+from models.amenity import Amenity
+
 
 app = Flask(__name__)
 
@@ -58,14 +61,40 @@ def number_odd_or_even(n):
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ states list """
+    """ def doc """
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ def doc """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def states(id=None):
+    """ def states """
+    states = storage.all(State)
+    if id is not None:
+        id = "State." + id
+    return render_template("9-states.html", states=states, state_id=id)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """ def hbnb """
+    states = storage.all(State)
+    Amenties = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html', states=states,
+                           Amenties=Amenties)
+
+
 @app.teardown_appcontext
-def close(error):
-    """ close """
+def tear_db(e):
+    """ tear db """
     storage.close()
 
 
